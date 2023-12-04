f679bbcec9466ab9689a548820481b7e5ca80f89
Timm Kent <marcfelden@me.com>
2023-12-04 12:36:44 +0100

    initial

---
+++ android/app/src/main/kotlin/com/example/tkinreview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tkinreview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/tkinreview.dart
@@ -0,0 +1,7 @@
+class TKInReview {
+  static final TKInReview _singleton = TKInReview._internal();
+  TKInReview._internal();
+  factory TKInReview() {
+    return _singleton;
+  }
+}
