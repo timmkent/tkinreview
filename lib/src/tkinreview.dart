32dd8ebc0962064c52da201489e3547bf2cafc8e
Timm Kent <marcfelden@me.com>
2024-01-15 15:31:06 +0100

    inReview fallback no network

--- lib/src/tkinreview.dart
+++ lib/src/tkinreview.dart
@@ -12,15 +12,19 @@ class TKInReview {
   bool? _isInReview;
 
   static initialize({required String appShort, required FirebaseDatabase firebaseDatabase}) async {
-    final snap = await firebaseDatabase.ref('underReview').child('underReview').get();
-    final swfForce = snap.value as bool? ?? false;
-    if (swfForce) return true;
-    final inReviewVersionsSnap = await TKDatabase.configs().ref(appShort).child('inReviewVersion').get();
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    final currentVersion = packageInfo.version;
-    final inReviewVersion = inReviewVersionsSnap.value as String;
-    print("CurrentVersion:$currentVersion  inReviewVersion:$inReviewVersion");
-    _singleton._isInReview = inReviewVersion == currentVersion;
+    try {
+      final snap = await firebaseDatabase.ref('underReview').child('underReview').get();
+      final swfForce = snap.value as bool? ?? false;
+      if (swfForce) return true;
+      final inReviewVersionsSnap = await TKDatabase.configs().ref(appShort).child('inReviewVersion').get();
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      final currentVersion = packageInfo.version;
+      final inReviewVersion = inReviewVersionsSnap.value as String;
+      print("CurrentVersion:$currentVersion  inReviewVersion:$inReviewVersion");
+      _singleton._isInReview = inReviewVersion == currentVersion;
+    } catch (_) {
+      _singleton._isInReview = true;
+    }
   }
 
   static bool get isInReview {
